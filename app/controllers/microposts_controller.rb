39d75ef5c3fc09dfe435e450024c05f62ce8312e
koichi.hagura <hagura.ftbl.tsukuba@outlook.jp>
2020-09-25 09:23:31 +0000

    review2

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :edit]
 
   def create
     @team = Team.find(params[:micropost][:team_id])
