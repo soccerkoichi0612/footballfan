6e56cb6b55c24ba58a70ada0a982658be4460e54
koichi.hagura <hagura.ftbl.tsukuba@outlook.jp>
2020-09-28 07:38:26 +0000

    review3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
-    team = Team.find(params[:team_id])
+    team = Team.find(params[:favorite][:team_id])
     current_user.favorite(team)
     flash[:success] = 'お気に入りに追加しました。'
     redirect_to likes_user_url(current_user)
   end
 
   def destroy
-    team = Team.find(params[:team_id])
+    team = Team.find(params[:favorite][:team_id])
     current_user.unfavorite(team)
     flash[:success] = 'お気に入りからはずしました。'
     redirect_to likes_user_url(current_user)
