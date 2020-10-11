8f06dad34ce48ee9e9ca30b6020ea339d83829f8
koichi.hagura <hagura.ftbl.tsukuba@outlook.jp>
2020-10-11 05:29:29 +0000

    toppage timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,7 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
+    end
   end
 end
